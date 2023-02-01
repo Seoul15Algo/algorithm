90515756038066dcc1ef808c0d435c7ef09fc314
soyun <soun997@gmail.com>
2023-02-01 09:10:47 +0900

    solve(1138, 14503, 14719): soyun_230131

--- soyun/Main_1138.java
+++ soyun/Main_1138.java
@@ -1,53 +1,48 @@
+package com.ssafy.baekjoon.implementation;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main_1138 {
-	
-	static int n;
-	static List<Integer> init = new ArrayList<>();
-	static List<Integer> nums = new ArrayList<>();
 
-	public static void main(String[] args) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		init.add(n);
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			nums.add(Integer.parseInt(st.nextToken()));
-		}
-		
-		for (int i = n - 2; i >= 0; i--) {
-			int cur = nums.get(i);
-			List<Integer> left = getLeft(cur);
-			List<Integer> right = getRight(cur);
-			init.clear();
-			init.addAll(left);
-			init.add(i + 1);
-			init.addAll(right);
-		}
-		
-		for (int i : init) {
-			System.out.print(i + " ");
-		}
-	}
-	
-	static List<Integer> getLeft(int cnt) {
-		List<Integer> temp = new ArrayList<>();
-		for (int i = 0; i < cnt; i++) {
-			temp.add(init.get(i));
-		}
-		return temp;
-	}
-	
-	static List<Integer> getRight(int start) {
-		List<Integer> temp = new ArrayList<>();
-		for (int i = start; i < init.size(); i++) {
-			temp.add(init.get(i));
-		}
-		return temp;
-	}
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] inputs = new int[n];
+        int[] result = new int[n];
+        boolean[] check = new boolean[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            inputs[i] = Integer.parseInt(st.nextToken());
+            result[i] = 1;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int cnt = inputs[i];
+            for (int j = 0; j < n; j++) {
+                if (cnt== 0) {
+                    for (int k = j; k < n; k++) {
+                        if (!check[k]) {
+                            check[k] = true;
+                            break;
+                        }
+                    }
+                    for (int k = j + 1; k < n; k++) {
+                        if (!check[k]) {
+                            result[k]++;
+                        }
+                    }
+                    break;
+                }
+                if (!check[j]) {
+                    result[j]++;
+                    cnt--;
+                }
+            }
+        }
+        for (int i : result)
+            System.out.print(i + " ");
+    }
 }
--- soyun/Main_14719.java
+++ soyun/Main_14719.java
@@ -1,3 +1,5 @@
+package com.ssafy.baekjoon.implementation;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
