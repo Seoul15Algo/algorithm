35accc4e8f3783182305a4b94cf96ee168affe59
sooyeon990828 <sooyeon990828@naver.com>
2023-03-08 21:37:41 +0900

    sort_fix

--- sooyeon/week6/QuickSort.java
+++ sooyeon/week6/QuickSort.java
@@ -14,36 +14,31 @@ public class QuickSort {
 
     // 구현 - 필요하면 매개변수와 메서드를 추가해도 됩니다.
     public void sort() {
-    	quickSort(0, array.length -1);
+    	quickSort(0,array.length);
     }
+
     public void quickSort(int start, int end) {
-        if (start >= end)
-          return;
+    	if(start+1>=end) return;
+		int pivot = array[start];
+		int left = start+1;
+		int right = end-1;
+		while(true) {
+			while(left<=right && array[left]<=pivot) left++;
+			while(left<=right && array[right] >= pivot) right--;
+			if(left>right) break;
+			swap(left,right);
+		}
+		swap(start,right);
+		quickSort(start,right);
+		quickSort(right+1,end);
+	}
 
-        int pivot = start;
-        int lo = start + 1;
-        int hi = end;
-        
-        while (lo <= hi) {
-          while (lo <= end && array[lo] <= array[pivot]) lo++;
-          while (hi > start && array[hi] >= array[pivot]) hi--;
-          if (lo > hi)
-            swap(hi, pivot);
-          else
-            swap(lo, hi);
-          }
-    	
-        quickSort(start, hi - 1);
-        quickSort(hi + 1, end);
-
-      }
-
-      public void swap(int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-    //
+	public void swap(int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+		
+	}
 
     public void printArray() {
         System.out.println(Arrays.toString(array));
