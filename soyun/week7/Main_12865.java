a224c9d661ed2620a1970cb4e224352c3b7544d3
소윤파크 <soun997@gmail.com>
2023-03-14 22:31:41 +0900

    solve(1309, 9251, 12865, 14002): soyun_230314

--- soyun/week7/Main_12865.java
+++ soyun/week7/Main_12865.java
@@ -14,7 +14,7 @@ public class Main_12865 {
         int k = Integer.parseInt(st.nextToken());
         int[] weights = new int[n + 1];
         int[] values = new int[n + 1];
-        int[] dp = new int[k + 1];
+        int[] dp = new int[k + 1]; // 배열 index만큼의 무게를 들 수 있을 때, 최대의 가치
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             // 짐의 무게
@@ -22,10 +22,14 @@ public class Main_12865 {
             // 짐의 가치
             values[i] = Integer.parseInt(st.nextToken());
         }
+        // 1번 짐부터 넣어본다.
         for (int i = 1; i <= n; i++) {
-            // 가방의 수용량을 1씩 늘린다
+            
+            // 담을 수 있는 무게의 한계치를 넘지 않을 때까지 반복
+            // ex) weight[i] = 3 (i번째 짐의 무게가 3), k = 7 이라면,
+            // 7, 6, 5, 4, 3까지 반복 -> 2부터는 i번째 짐을 수용할 수 없으므로 제외 (반복 횟수 줄이기)
             for (int j = k; j - weights[i] >= 0; j--) {
-                // 수용량이 k일 때, i번째 짐을 넣지 않을 때의 최대 가치와 넣었을 때의 최대 가치를 비교
+                // 최대 가치를 갱신해준다
                 dp[j] = Math.max(dp[j], dp[j - weights[i]] + values[i]);
             }
         }
