6757bc5dd3a416df4d64f3953901e0fcbef5ee55
rt3310 <btac3310@gmail.com>
2023-04-19 18:26:10 +0900

    solve(11066)

--- wonho/week12/Q11066.java
+++ wonho/week12/Q11066.java
@@ -3,7 +3,6 @@ package week12;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q11066 {
@@ -11,8 +10,9 @@ public class Q11066 {
     private static BufferedReader br;
     private static StringBuilder sb;
     private static int[] files;
+    private static long[] fileSum;
+    private static long[][] dp;
     private static int n;
-
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
@@ -28,33 +28,63 @@ public class Q11066 {
     public static void testcase() throws IOException {
         n = Integer.parseInt(br.readLine());
         files = new int[n];
-        int[] dp = new int[n];
+        fileSum = new long[n];
+        dp = new long[n][n];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             files[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(search(0, n - 1));
-        System.out.println(Arrays.toString(dp));
-    }
+        fileSum[0] = files[0];
+        for (int i = 1; i < n; i++) {
+            fileSum[i] = files[i] + fileSum[i - 1];
+        }
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = files[i];
+        }
 
+        sb.append(search(0, n - 1)).append("\n");
+    }
+    
     public static long search(int left, int right) {
-        if (left > right || left >= n) {
-            return Integer.MAX_VALUE;
+        if (dp[left][right] != 0) {
+            return dp[left][right];
+        }
+
+        if (right - left == 1) {
+            return files[left] + files[right];
         }
 
         if (left == right) {
-            System.out.println("left = " + left);
-            System.out.println("files = " + files[left]);
             return files[left];
         }
 
-        System.out.println("left, right = " + left + ", " + right);
         long min = Integer.MAX_VALUE;
         for (int i = left; i < right; i++) {
-            min = Math.min(min, search(left, i) + search(i + 1, right));
+            long leftTotal = search(left, i);
+            long rightTotal = search(i + 1, right);
+
+            long leftSum = fileSum[i] - (left == 0 ? 0 : fileSum[left - 1]);
+            long rightSum = fileSum[right] - fileSum[i];
+
+            if (i == left && i + 1 == right) {
+                return leftTotal + rightTotal;
+            }
+
+            if (i == left) {
+                min = Math.min(min, leftSum + rightSum + rightTotal);
+                continue;
+            }
+
+            if (i + 1 == right) {
+                min = Math.min(min, leftSum + rightSum + leftTotal);
+                continue;
+            }
+
+            min = Math.min(min, leftSum + rightSum + leftTotal + rightTotal);
         }
 
-        return min;
+        return dp[left][right] = min;
     }
 }
