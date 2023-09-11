0ca27a370b939e9f20a0403a90464ce7e31c86f7
sypark <soun997@gmail.com>
2023-09-12 03:54:39 +0900

    solve(프렌즈4블록, 압축): soyun_230912

--- soyun/week27/프렌즈4블록.java
+++ soyun/week27/프렌즈4블록.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 class Solution {
-
+    
     private char[][] board;
     private int popped;
-
+    
     public int solution(int m, int n, String[] input) {
         board = new char[m][n];
         popped = 0;
@@ -16,11 +16,14 @@ class Solution {
         }
         return popped;
     }
-
+    
     public boolean pop(int m, int n) {
         boolean isPopped = false;
         for (int i = 0; i < m - 1; i++) {
             for (int j = 0; j < n - 1; j++) {
+                if (board[i][j] == ' '){
+                    continue;
+                }
                 if (isSame(i, j)) {
                     board[i][j] = Character.toLowerCase(board[i][j]);
                     board[i + 1][j] = Character.toLowerCase(board[i + 1][j]);
@@ -32,18 +35,15 @@ class Solution {
         }
         return isPopped;
     }
-
+    
     public boolean isSame(int x, int y) {
-        if (board[x][y] == ' ') {
-            return false;
-        }
         char a = Character.toUpperCase(board[x][y]);
         char b = Character.toUpperCase(board[x + 1][y]);
         char c = Character.toUpperCase(board[x][y + 1]);
         char d = Character.toUpperCase(board[x + 1][y + 1]);
         return (a == b) && (a == c) && (a == d);
     }
-
+    
     public void drag(int m, int n) {
         for (int i = m - 1; i >= 0; i--) {
             for (int j = 0; j < n; j++) {
@@ -52,18 +52,13 @@ class Solution {
                     popped++;
                     continue;
                 }
-                if (i + 1 >= m) {
-                    continue;
-                }
-                if (Character.isUpperCase(board[i + 1][j])) {
-                    continue;
-                }
-                for (int next = i + 2; next < m; next++) {
-                    if (Character.isUpperCase(board[next][j])) {
-                        board[next - 1][j] = board[i][j];
-                        board[i][j] = ' ';
-                        break;
+                int row = i + 1;
+                while(row < m) {
+                    if (board[row][j] == ' ') {
+                        board[row][j] = board[row - 1][j];
+                        board[row - 1][j] = ' ';
                     }
+                    row++;
                 }
             }
         }
